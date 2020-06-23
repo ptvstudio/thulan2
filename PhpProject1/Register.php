eea672a86430d1b9bb318be720ebac9a5e8ffccd
ASUS <ASUS@LAPTOP-Q0AGKP11>
2020-06-23 21:10:07 +0700

    THem link

--- PhpProject1/Register.php
+++ PhpProject1/Register.php
@@ -1,129 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Register</title> 
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<script language="javascript" src="Register.js"></script>    
-</head>
-<body>
-<div class="top-nav-bar">
-<div class="search-box">
-<i class="fa fa-bars" id="menu-btn" onclick="openmenu()"></i>
-<i class="fa fa-bars" id="close-btn" onclick="closemenu()"></i>
-<a href="PTVOnline.php"><img src="Images/Logo.png" class="logo"></a>
-<input type=text class="form-control">
-<span class="input-group-text"><i class="fa fa-search"></i></span>          
-</div>
-</div>
-<div class="menu-bar">
-<ul>
-<li><a href="#"><i class="fa fa-shopping-basket"></i>Cart</a></li>
-<li><a href="#">Register</a></li>
-<li><a href="#">Log In</a></li>
-</ul>    
-</div>
-<form action="" method="POST" onsubmit="return validate()">
-<div class="register">
-<h1>Register</h1>
-<p>Please fill in the information to register</p>
-<hr>
-<label for="username"><b>Username</b></label>
-<input type="text" placeholder="username" name="username" id="username">
-<label for="password"><b>Password</b></label>
-<input type="password" placeholder="******" name="password" id="password">
-<label for="password-repeat"><b>Repeat your password</b></label>
-<input type="password" placeholder="******" name="password-repeat" id="password-repeat">
-<hr>
-<p>To create an account please agree to our terms <a href="#">Terms &amp; Privacy</a>.</p>
-<button type="submit" class="submit">Submit</button>
-</div>
-<div class="register login">
-<p>Already have an account <a href="#">Log in</a>.</p>
-</div>
-</form>
-   <section class="footer">
-    <div class="container text-center">
-    <div class="row">
-    <div class="col-md-3">
-        <h1>Useful Links</h1>
-        <p>Privacy Policy</p>
-        <p>Terms of use</p>
-        <p>Return Policy</p>
-        <p>Discount Coupons</p>
-    </div> 
-    
-        <div class="col-md-3">
-        <h1>Company</h1>
-        <p>About Us</p>
-        <p>Contact Us</p>
-        <p>Career</p>
-        <p>Affiliate</p>
-    </div> 
-        <div class="col-md-3">
-        <h1>Follow Us On</h1>
-        <p><i class="fa fa-facebook-official"></i> Facebook</p>
-        <p><i class="fa fa-youtube-play"></i> Youtube</p>
-        <p><i class="fa fa-twitter"></i> Twitter</p>
-        <p><i class="fa fa-instagram"></i> Instagram</p>
-    </div> 
-        <div class="col-md-3 footer-image">
-        <h1>Download App</h1>
-            <img src="Images/Download.png">
-        </div>
-    </div>
-        <hr>
-        <p class="copyright"> Made with <i class="fa fa-heart-o"></i> by PTV</p>
-    </div>
-    </section>
-<script>
-    function openmenu()
-    {
-        document.getElementById("side-menu").style.display="block";
-        document.getElementById("menu-btn").style.display="none";
-        document.getElementById("close-btn").style.display="block";
-    }
-    function closemenu()
-    {
-        document.getElementById("side-menu").style.display="none";
-        document.getElementById("menu-btn").style.display="block";
-        document.getElementById("close-btn").style.display="none";
-    }
-    </script>    
-    </body>
-</html>    
 <?php
-// Include config file
+
 require_once "config.php";
  
-// Define variables and initialize with empty values
+
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
  
-// Processing form data when form is submitted
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Validate username
+   
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } else{
-        // Prepare a select statement
+       
         $sql = "SELECT id FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
+           
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
+        
             $param_username = trim($_POST["username"]);
             
-            // Attempt to execute the prepared statement
+            
             if(mysqli_stmt_execute($stmt)){
-                /* store result */
+               
                 mysqli_stmt_store_result($stmt);
                 
                 if(mysqli_stmt_num_rows($stmt) == 1){
@@ -135,12 +37,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
+           
             mysqli_stmt_close($stmt);
         }
     }
     
-    // Validate password
+  
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";     
     } elseif(strlen(trim($_POST["password"])) < 6){
@@ -149,7 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $password = trim($_POST["password"]);
     }
     
-    // Validate confirm password
+    
     if(empty(trim($_POST["confirm_password"]))){
         $confirm_password_err = "Please confirm password.";     
     } else{
@@ -159,34 +61,74 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
     
-    // Check input errors before inserting in database
+    
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
-        // Prepare an insert statement
+       
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
+            
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
             
-            // Set parameters
+         
             $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_password = password_hash($password, PASSWORD_DEFAULT); 
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
+          
+               
                 header("location: login.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
 
-            // Close statement
+           
             mysqli_stmt_close($stmt);
         }
     }
     
-    // Close connection
+    
     mysqli_close($link);
 }
 ?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
