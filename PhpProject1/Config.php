a379f3edc8b9b469e62ff44e482dfb7d05124537
ASUS <ASUS@LAPTOP-Q0AGKP11>
2020-06-23 21:38:19 +0700

    THem link

--- PhpProject1/Config.php
+++ PhpProject1/Config.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'users');
-define('DB_PASSWORD', 'Vupro1234');
-define('DB_NAME', 'users');
+<?php
+
+define('DB_SERVER', 'ec2-35-172-73-125.compute-1.amazonaws.com');
+define('DB_USERNAME', 'd120l5u259o1la');
+define('DB_PASSWORD', '25ffc0cd7a2f79a8d6cc7e1d560a8f4ae1da44f7dfcfae4a795b7c3fe3cf3915');
+define('DB_NAME', 'd120l5u259o1la');
+ 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
-    </body>
-</html>
