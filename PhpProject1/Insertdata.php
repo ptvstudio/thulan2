5d94d9373af3e74861ddde6217fe40b2f30d93f7
ASUS <ASUS@10.25.89.185>
2020-06-24 07:52:29 +0700

    THem link

--- PhpProject1/Insertdata.php
+++ PhpProject1/Insertdata.php
@@ -39,7 +39,7 @@ list-style: none;
 <h1>INSERT DATA TO DATABASE</h1>
 <h2>Enter data into product table</h2>
 <ul>
-    <form name="Insertdata" action="Insertdata.php" method="POST" >
+<form name="Insertdata" action="Insertdata.php" method="POST" >
 <li>Product ID:</li><li><input type="text" name="productid" /></li>
 <li>Product Name:</li><li><input type="text" name="productname" /></li>
 <li>Size:</li><li><input type="text" name="sizes" /></li>
@@ -48,6 +48,49 @@ list-style: none;
 <li><input type="submit" /></li>
 </form>
 </ul>
+
+
+<?php
+
+if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=users', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+         "host=
+ec2-35-172-73-125.compute-1.amazonaws.com
+;port=5432;user=tjdbbuqrfdlyyq;password=25ffc0cd7a2f79a8d6cc7e1d560a8f4ae1da44f7dfcfae4a795b7c3fe3cf3915;dbname=d120l5u259o1la",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+if($pdo === false){
+     echo "ERROR: Could not connect Database";
+}
+
+
+$sql = "INSERT INTO product(productid, productname, sizes, basicprice, residual)" 
+        . " VALUES('$_POST[productid]','$_POST[productname]','$_POST[sizes]','$_POST[basicprice]','$_POST[residual]')";
+$stmt = $pdo->prepare($sql);
+//$stmt->execute();
+ if (is_null($_POST[productid])) {
+   echo "ProductID must be not null";
+ }
+ else
+ {
+    if($stmt->execute() == TRUE){
+        echo "Record inserted successfully.";
+    } else {
+        echo "Error inserting record: ";
+    }
+ }
+?>
 <section class="footer">
     <div class="container text-center">
     <div class="row">
@@ -96,47 +139,5 @@ list-style: none;
         document.getElementById("close-btn").style.display="none";
     }
     </script>    
-
-<?php
-
-if (empty(getenv("DATABASE_URL"))){
-    echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=users', 'postgres', '123456');
-}  else {
-     
-   $db = parse_url(getenv("DATABASE_URL"));
-   $pdo = new PDO("pgsql:" . sprintf(
-         "host=
-ec2-35-172-73-125.compute-1.amazonaws.com
-;port=5432;user=tjdbbuqrfdlyyq;password=25ffc0cd7a2f79a8d6cc7e1d560a8f4ae1da44f7dfcfae4a795b7c3fe3cf3915;dbname=d120l5u259o1la",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-   ));
-}  
-
-if($pdo === false){
-     echo "ERROR: Could not connect Database";
-}
-
-
-$sql = "INSERT INTO product(productid, productname, sizes, basicprice, residual)" 
-        . " VALUES('$_POST[productid]','$_POST[productname]','$_POST[sizes]','$_POST[basicprice]','$_POST[residual]')";
-$stmt = $pdo->prepare($sql);
-//$stmt->execute();
- if (is_null($_POST[productid])) {
-   echo "ProductID must be not null";
- }
- else
- {
-    if($stmt->execute() == TRUE){
-        echo "Record inserted successfully.";
-    } else {
-        echo "Error inserting record: ";
-    }
- }
-?>
 </body>
 </html>
