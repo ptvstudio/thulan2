5902d2b8a03ec615597f32c8f38b42eca8cf6636
ASUS <ASUS@10.25.89.185>
2020-06-24 07:49:56 +0700

    THem link

--- PhpProject1/Insertdata.php
+++ PhpProject1/Insertdata.php
@@ -1,6 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script language="javascript" src="Register.js"></script>    
+</head>
+<body>
+<div class="top-nav-bar">
+<div class="search-box">
+<i class="fa fa-bars" id="menu-btn" onclick="openmenu()"></i>
+<i class="fa fa-bars" id="close-btn" onclick="closemenu()"></i>
+<a href="PTVOnline.php"><img src="Images/Logo.png" class="logo"></a>
+<input type=text class="form-control">
+<span class="input-group-text"><i class="fa fa-search"></i></span>          
+</div>
+</div>
+<div class="menu-bar">
+<ul>
+<li><a href="#"><i class="fa fa-shopping-basket"></i>Cart</a></li>
+<li><a href="#">Register</a></li>
+<li><a href="#">Log In</a></li>
+</ul>    
+</div>
 <title>Insert data to PostgreSQL with PHP</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
@@ -8,6 +33,7 @@ li {
 list-style: none;
 }
 </style>
+
 </head>
 <body>
 <h1>INSERT DATA TO DATABASE</h1>
@@ -22,6 +48,54 @@ list-style: none;
 <li><input type="submit" /></li>
 </form>
 </ul>
+<section class="footer">
+    <div class="container text-center">
+    <div class="row">
+    <div class="col-md-3">
+        <h1>Useful Links</h1>
+        <p>Privacy Policy</p>
+        <p>Terms of use</p>
+        <p>Return Policy</p>
+        <p>Discount Coupons</p>
+    </div> 
+    
+        <div class="col-md-3">
+        <h1>Company</h1>
+        <p>About Us</p>
+        <p>Contact Us</p>
+        <p>Career</p>
+        <p>Affiliate</p>
+    </div> 
+        <div class="col-md-3">
+        <h1>Follow Us On</h1>
+        <p><i class="fa fa-facebook-official"></i> Facebook</p>
+        <p><i class="fa fa-youtube-play"></i> Youtube</p>
+        <p><i class="fa fa-twitter"></i> Twitter</p>
+        <p><i class="fa fa-instagram"></i> Instagram</p>
+    </div> 
+        <div class="col-md-3 footer-image">
+        <h1>Download App</h1>
+            <img src="Images/Download.png">
+        </div>
+    </div>
+        <hr>
+        <p class="copyright"> Made with <i class="fa fa-heart-o"></i> by PTV</p>
+    </div>
+    </section>
+<script>
+    function openmenu()
+    {
+        document.getElementById("side-menu").style.display="block";
+        document.getElementById("menu-btn").style.display="none";
+        document.getElementById("close-btn").style.display="block";
+    }
+    function closemenu()
+    {
+        document.getElementById("side-menu").style.display="none";
+        document.getElementById("menu-btn").style.display="block";
+        document.getElementById("close-btn").style.display="none";
+    }
+    </script>    
 
 <?php
 
